ad72a054f4b0cf989e29ed3e7e04a266d6c8dfb4
Art1shz <art1shz@mail.ru>
2023-03-30 02:15:15 +0300

    prac8

---
+++ prac8/Program.cs
@@ -0,0 +1,12 @@
+﻿int node = Convert.ToInt32(Console.ReadLine());
+int edge = Convert.ToInt32(Console.ReadLine());
+Dictionary<int, List<int>> Dictionary = new Dictionary<int, List<int>>();
+for (int i = 1; i < node + 1; i++) Dictionary.Add(i, new List<int>());
+for (int i = 0; i < edge; i++)
+{
+    int vertexOne = Convert.ToInt32(Console.ReadLine());
+    int vertexTwo = Convert.ToInt32(Console.ReadLine());
+    Dictionary[vertexOne].Add(vertexTwo);
+    Dictionary[vertexTwo].Add(vertexOne);
+}
+foreach (var item in Dictionary.Values) Console.WriteLine(String.Join(" ", item));
